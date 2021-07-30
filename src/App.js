a1c84e40d0dd1b2cd0d13e438e195aa3fae26c04
ASdeveloper-app <arturo.araujo.alvarez@gmail.com>
2021-07-29 19:29:07 -0500

    mini

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./config.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="App-link">PROXIMAMENTE</p>
-        <p>Sitio en construcción</p>
+        <img src="inverna_logotipo.svg" alt="logo" />
+        <p>Invernaderos del Sur</p>
+        <p>Mejorando para ti.</p>
+        <p className="App-link">Espéralo</p>
       </header>
     </div>
   );
